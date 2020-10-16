03d2c942a490c60c62ec8a7c4ddf703b54d8bdb4
Dimitri Korsch <dimitri.korsch@uni-jena.de>
2020-10-16 15:10:47 +0200

    pretified FT script

--- example/run_ft.sh
+++ example/run_ft.sh
@@ -1,2 +1,19 @@
-for big in 0 1; do for mt in inception inception_imagenet resnet; do for ds in CUB200 NAB BIRDSNAP; do OUTPUT_PREFIX=.results_ft_BIG${big} BIG=$big DATASET=$ds MODEL_TYPE=$mt BATCH_SIZE=24 ./train.sh ; done ; done ; done
+for run in $(seq 1 5);
+do
+	for big in 0 1;
+	do
+		for mt in inception inception_imagenet resnet;
+		do
+			for ds in CUB200 NAB BIRDSNAP;
+			do
+				OUTPUT_PREFIX=.results_ft_BIG${big} \
+				BIG=$big \
+				DATASET=$ds \
+				MODEL_TYPE=$mt \
+				BATCH_SIZE=24 \
+					./train.sh
+			done
+		done
+	done
+done
 
