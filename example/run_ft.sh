6e5e3840e545b961166f812a078f5e5b9df73b8e
Dimitri Korsch <dimitri.korsch@uni-jena.de>
2020-10-20 10:09:14 +0200

    updated ft script and conda env file

--- example/run_ft.sh
+++ example/run_ft.sh
@@ -1,21 +1,58 @@
+#!/usr/bin/env bash
+
+CLUSTER=${CLUSTER:-0}
+
+if [[ ${CLUSTER} != 0 ]]; then
+	echo "=== CLUSTER execution enabled! ==="
+	export OPTS=${OPTS:-"--no_progress"}
+
+	NODE=${NODE:-s_mgx1,gpu_p100,gpu_v100}
+
+	SBATCH_OPTS="${SBATCH_OPTS} --gres gpu:1"
+	SBATCH_OPTS="${SBATCH_OPTS} -c 3"
+	SBATCH_OPTS="${SBATCH_OPTS} --mem 64G"
+	SBATCH_OPTS="${SBATCH_OPTS} -p ${NODE}"
+
+	SBATCH_OUTPUT=${SBATCH_OUTPUT:-".sbatch/$(date +%Y-%m-%d_%H.%M.%S)"}
+	mkdir -p $SBATCH_OUTPUT
+	SBATCH_OPTS="${SBATCH_OPTS} --output ${SBATCH_OUTPUT}/fve_layer.%A.out"
+
+	echo "slurm outputs will be saved under ${SBATCH_OUTPUT}"
+fi
+
+MODELS=${MODELS:-"inception inception_imagenet resnet"}
+DATASETS=${DATASETS:-"CUB200 NAB BIRDSNAP"}
+
+export BATCH_SIZE=24
+export FVE_TYPE=no
+export PARTS=GLOBAL
+export N_JOBS=4
+
 for run in $(seq 1 5);
 do
 	for big in 0 1;
 	do
-		for mt in inception inception_imagenet resnet;
+		for mt in $MODELS;
 		do
-			for ds in CUB200 NAB BIRDSNAP;
+			for ds in $DATASETS;
 			do
+
+				JOB_NAME=fve_BIG${big}_${ds}_${mt}
+
+				if [[ ${CLUSTER} != 0 ]]; then
+					SBATCH="sbatch --job-name ${JOB_NAME} ${SBATCH_OPTS}"
+				fi
+
 				OUTPUT_PREFIX=.results_ft_BIG${big} \
 				BIG=$big \
-				PARTS=GLOBAL \
-				FVE_TYPE=no \
 				DATASET=$ds \
 				MODEL_TYPE=$mt \
-				BATCH_SIZE=24 \
-					./train.sh
+				${SBATCH} ./train.sh $@
 			done
 		done
 	done
 done
 
+if [[ ${CLUSTER} != 0 ]]; then
+	rmdir --ignore-fail-on-non-empty ${SBATCH_OUTPUT}
+fi
