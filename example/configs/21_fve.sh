e0208d678c086a776d2f5a50586f9119560e2966
Dimitri Korsch <dimitri.korsch@uni-jena.de>
2020-10-21 13:37:54 +0200

    minor changes in the fve default settings

--- example/configs/21_fve.sh
+++ example/configs/21_fve.sh
@@ -1,8 +1,8 @@
-FVE_TYPE=${FVE_TYPE:-em} # 'no', 'grad', or 'em'
+FVE_TYPE=${FVE_TYPE:-no} # 'no', 'grad', or 'em'
 
 N_COMPONENTS=${N_COMPONENTS:-1}
 COMP_SIZE=${COMP_SIZE:-256}
-AUX_LAMBDA=${AUX_LAMBDA:-0.9}
+AUX_LAMBDA=${AUX_LAMBDA:-0.0}
 
 OPTS="${OPTS} --fve_type ${FVE_TYPE}"
 OPTS="${OPTS} --n_components ${N_COMPONENTS}"
