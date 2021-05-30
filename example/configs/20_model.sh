84303b36426a83853b2b4b3ef8ea3e4f081e86bb
Dimitri Korsch <dimitri.korsch@uni-jena.de>
2021-05-30 16:06:32 +0200

    added FP16 handling to the bash configs

--- example/configs/20_model.sh
+++ example/configs/20_model.sh
@@ -12,6 +12,10 @@ PARTS_INPUT_SIZE=${PARTS_INPUT_SIZE:-299}
 # concat mean
 FEATURE_AGG=${FEATURE_AGG:-concat}
 
+if [[ ! -z $FP16 ]]; then
+	OPTS="${OPTS} CHAINER_DTYPE=mixed16"
+fi
+
 case $MODEL_TYPE in
 	"cvmodelz.InceptionV3" | "chainercv2.inceptionv3" )
 		PARTS_INPUT_SIZE=299
