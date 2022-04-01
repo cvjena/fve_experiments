33215d388d715aa0c31674d862bb48cf06888177
Dimitri Korsch <dimitri.korsch@uni-jena.de>
2022-04-01 16:34:54 +0200

    added self-attention classifier

--- fgvc/fve_fgvc/core/model/classifier/__init__.py
+++ fgvc/fve_fgvc/core/model/classifier/__init__.py
@@ -1,8 +1,10 @@
 from fve_fgvc.core.model.classifier.glob_clf import GlobalClassifier
+from fve_fgvc.core.model.classifier.glob_clf import SelfAttentionClassifier
 from fve_fgvc.core.model.classifier.part_clf import PartsClassifier
 
 
 __all__ = [
 	"GlobalClassifier",
+	"SelfAttentionClassifier",
 	"PartsClassifier",
 ]
