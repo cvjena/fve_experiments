e1ee7e40b4e7adcc750a82d3a8ddef4bf93b5e0f
Dimitri Korsch <dimitri.korsch@uni-jena.de>
2022-04-01 09:16:59 +0200

    added settable pre-training option in the config scripts

--- fgvc/scripts/configs/20_model.sh
+++ fgvc/scripts/configs/20_model.sh
@@ -3,7 +3,6 @@ GPU=${GPU:-0}
 
 # MODEL_TYPE=${MODEL_TYPE:-chainercv2.resnet50}
 MODEL_TYPE=${MODEL_TYPE:-cvmodelz.InceptionV3}
-PRE_TRAINING=${PRE_TRAINING:-imagenet}
 PREPARE_TYPE=${PREPARE_TYPE:-model}
 
 INPUT_SIZE=${INPUT_SIZE:-299}
@@ -19,7 +18,7 @@ fi
 case $MODEL_TYPE in
 	"cvmodelz.InceptionV3" | "chainercv2.inceptionv3" )
 		PARTS_INPUT_SIZE=299
-		PRE_TRAINING=inat
+		PRE_TRAINING=${PRE_TRAINING:-inat}
 		if [[ ${BIG:-0} == 0 ]]; then
 			INPUT_SIZE=299
 		elif [[ ${BIG:-0} == -1 ]]; then
@@ -53,6 +52,7 @@ esac
 
 LOAD=${LOAD:-""}
 WEIGHTS=${WEIGHTS:-""}
+PRE_TRAINING=${PRE_TRAINING:-imagenet}
 
 if [[ ! -z ${LOAD} ]]; then
 	if [[ ! -z ${WEIGHTS} ]]; then
